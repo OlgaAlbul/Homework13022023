dcbf5ba5c57f7eee6e2f415c6fff1c195e9c9b11
AlbulOlya <albul.olya@gmail.com>
2023-02-14 17:53:48 +0500

    Task 2 solved

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+void FillArray(int[,] matr)         
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j]=i+j;
+
+        }
+    }
+}
+
+void PrintArray(int[,] matr)       
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"\t {matr[i,j]} ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+Console.WriteLine("Enter the number of rows"!);
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the number of colomns"!);
+int n = int.Parse(Console.ReadLine());
+int[,] matrix = new int [m,n]; 
+FillArray(matrix);
+PrintArray(matrix);
